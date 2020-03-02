c2141879df756500d72e2de3f20906b6e8c5eb03
adoniramj <adoniramjvargas@gmail.com>
2020-03-01 21:51:24 -0500

    Initial setup for my first angular project. Made modifications to index.html, app.component.html, and app.component.ts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  name:string = "Adoniram J. Vargas";
+
+  constructor() {
+    console.log('Hello World')
+    this.changeName('John Doe')
+  }
+
+  changeName(name:string):void {
+    this.name = name
+  }
 }
